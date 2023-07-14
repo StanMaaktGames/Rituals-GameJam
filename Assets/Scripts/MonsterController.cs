b7e5527e9a3e275d3ca6cf1beb783c7b4d3eb84d
StanMaaktGames <100526568+StanMaaktGames@users.noreply.github.com>
2023-07-14 11:18:46 +0200

    Project geupdate

---
+++ Assets/Scripts/MonsterController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class MonsterController : MonoBehaviour
+{
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+    public ThirdPersonCharacter character;
+
+    private void Start()
+    {
+        agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // MOVE AGENT
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+}
